31746918928f15acdb4212b06384fbd53c918f17
Madaki1234 <noblemadaki@gmail.com>
2025-04-10 00:42:20 +0000

    updated index.html and added main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "mainEntity": [
+    {
+      "@type": "Question",
+      "name": "What courses do you offer?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "We offer a host of frontend courses such as HTML, CSS, JavaScript, React, Next.js, Angular, and more."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "How can I become proficient in frontend development?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Firstly, understand how the web works. Master the fundamentals (HTML, CSS, JavaScript) of web development, and then build on that by learning other libraries and technologies involved in building web applications."
+      }
+    }
+  ]
+}
